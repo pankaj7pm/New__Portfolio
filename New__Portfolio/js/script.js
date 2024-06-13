3eaccc425b077f07243cbde68fb7ba582b375dca
Pankaj Modanwal <91431665+pankaj7pm@users.noreply.github.com>
2024-06-13 16:55:41 +0530

    Add files via upload

---
+++ New__Portfolio/js/script.js
@@ -0,0 +1,7 @@
+/* ================  typing animation ================ */
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Frontend Developer","Html Developer"],
+    typedSpeed:,
+    BackSpeed:1,
+    loop:true
+})
